e81d37327b6ec22f4e6ee79b96865d1c226a0809
Tatsuki Kitaoka <ksjfisa_sand@yahoo.co.jp>
2021-01-15 20:18:36 +0900

    Update Task_3-2 on Jan 15th to ask

--- js/main.js
+++ js/main.js
@@ -23,25 +23,37 @@
   };
 
   // ボタン生成の関数を定義
-  function createBottun(textButton) {
+  function createButton(textButton) {
     const button = document.createElement('button'); // ボタンの要素ノードを作成
     const textNodeButton = document.createTextNode(textButton); // "作業中"のテキストノードを作成
     button.appendChild(textNodeButton); // ボタンに"作業中"を表示
     return button;
   };
 
+  // 削除ボタンにClass名とidを付与する関数を定義
+  function addDeleteButtonId() {
+    const deletButton = createButton('削除');
+    deletButton.className = 'Delete';
+    deletButton.id = 'delete';
+    deletButton.onclick = clickDeleteButton();
+    // deletButton[0].setAttribute("id", id); //なぜエラー「Uncaught TypeError: Cannot read property 'setAttribute' of undefined」でる？
+    return deletButton;
+  };
+
   // 表示するノードを組み立て（ループ処理）の関数を定義
   function buildNodeByForeach(tempAry) {
     aryToDo.forEach(function (value, index) {
       const tr = document.createElement('tr'); // テーブルの行要素ノードtrを作成      
+      tr.className = 'ToDoList';
       tr.appendChild(createTd(index)); // IDの要素ノードをtrに追加      
       tr.appendChild(createTd(value.content)); // コメントの要素ノードをtrに追加
       // 状態ノードtrを作成
       const tdStatus = document.createElement('td'); // 状態の要素ノードを作成
       const textSpace = document.createTextNode('\t'); // スペース（空白）のテキストノードを作成
-      tdStatus.appendChild(createBottun('作業中')); // ノード同士の組み立て
+      tdStatus.appendChild(createButton('作業中')); // ノード同士の組み立て
       tdStatus.appendChild(textSpace); // ノード同士の組み立て
-      tdStatus.appendChild(createBottun('削除')); // ノード同士の組み立て
+      // tdStatus.appendChild(createButton('削除')); // ノード同士の組み立て
+      tdStatus.appendChild(addDeleteButtonId(index)); // ノード同士の組み立て
       tr.appendChild(tdStatus); // trに状態ボタンを追加
       // 作成した行ノードtrをテーブルTABLEに追加
       table.appendChild(tr);
@@ -60,5 +72,19 @@
       buildNodeByForeach(table);// 表示するノードを組み立て（ループ処理）      
       comment.value = '';// 入力テキストを空にする
     };
+    // console.dir(document.getElementsByClassName('delete')); // debug用
   });
+
+  // 削除ボタンClick時の処理
+  function clickDeleteButton() {
+      const deleteClass = document.getElementsByClassName('Delete');
+      for (let i = 0; i < deleteClass.length; i++) {
+        deleteClass[i].addEventListener('click', e => {
+          this.closest('.ToDoList[i]').remove();
+        });
+      }
+  };
+
+
+  
 }
